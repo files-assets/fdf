5f5dd25f14f7d1fc670ba9aba2255cc7ffaabed4
Luiz Felipe <frazaolipe@gmail.com>
2017-06-14 15:40:18 -0300

    Create form-replace.js

--- js/form-replace.js
+++ js/form-replace.js
@@ -7,14 +7,19 @@
     var $form = $('#fa-generated-form');
 
     $form.on('submit', function(event) {
+      
+      if ($textarea.length === 0 && $title.lenght === 0) {
+        alert('Tente novamente!');
+        return false;
+      }
 
       event.preventDefault();
 
-      $textarea.val($textarea.val().replace(/\{\{field(\d+)\}\}/gi, function(text, match) {
+      $textarea.val($textarea.val().replace(/\{\{campo(\d+)\}\}/gi, function(text, match) {
         return $('#campo' + match).val();
       }));
 
-      $title.val($title.val().replace(/\{\{field(\d+)\}\}/gi, function(text, match) {
+      $title.val($title.val().replace(/\{\{campo(\d+)\}\}/gi, function(text, match) {
         return $('#campo' + match).val();
       }));
     });
